f64a531933e6a791dfb95380350f2f668c134014
Henrik Storch <thisisthefoxe@gmail.com>
2022-03-12 07:13:56 +0100

    Initial Commit

---
+++ DaysSinceLast/DaysSinceLastApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysSinceLastApp.swift
+//  DaysSinceLast
+//
+//  Created by Henrik Storch on 12.03.22.
+//
+
+import SwiftUI
+
+@main
+struct DaysSinceLastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
